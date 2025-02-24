2763216e78970101a82041650c6b7842eb72ef5d
wxinix <xinwuping007@gmail.com>
2025-02-24 17:08:28 -0500

    Update delegate and delegate test

--- tests/delegate_test.cpp
+++ tests/delegate_test.cpp
@@ -7,73 +7,70 @@ TEST_SUITE_BEGIN("TEST_SUITE vtc::delegate");
 
 using namespace vtc::delegate;
 
-bool compare(int a, int b)
+bool compare(const int a, const int b)
 {
-  return a == b;
+    return a == b;
 }
 
 class MyTestClass
 {
 public:
-  bool compare(int a, int b)
-  {
-    data_++;
-    return a == b;
-  }
+    bool compare(const int a, const int b)
+    {
+        data_++;
+        return a == b;
+    }
 
-  [[nodiscard]] bool compare_const(int a, int b) const
-  {
-    return (a + data_) == b;
-  }
+    [[nodiscard]] bool compare_const(int a, int b) const
+    {
+        return (a + data_) == b;
+    }
 
 private:
-  int data_{0};
+    int data_{0};
 };
 
 TEST_CASE("TEST_CASE - global function")
 {
-  auto delegated_compare = delegate(compare);
-  CHECK(delegated_compare(5, 5));
-  CHECK_FALSE(delegated_compare(5, 6));
+    auto delegated_compare = delegate(compare);
+    CHECK(delegated_compare(5, 5));
+    CHECK_FALSE(delegated_compare(5, 6));
 }
 
 TEST_CASE("TEST_CASE - class member function")
 {
-  MyTestClass test_obj;
+    MyTestClass test_obj;
 
-  SUBCASE("Can delegate non-constant member function")
-  {
-    auto delegated_compare = delegate(&test_obj, &MyTestClass::compare);
-    CHECK(delegated_compare(5, 5));
-    CHECK_FALSE(delegated_compare(5, 6));
-  }
+    SUBCASE("Can delegate non-constant member function")
+    {
+        auto delegated_compare = delegate(&test_obj, &MyTestClass::compare);
+        CHECK(delegated_compare(5, 5));
+        CHECK_FALSE(delegated_compare(5, 6));
+    }
 
-  SUBCASE("Can delegate constant member function")
-  {
-    auto delegated_compare = delegate(&test_obj, &MyTestClass::compare_const);
-    CHECK(delegated_compare(5, 5));
-    CHECK_FALSE(delegated_compare(5, 6));
-  }
+    SUBCASE("Can delegate constant member function")
+    {
+        auto delegated_compare = delegate(&test_obj, &MyTestClass::compare_const);
+        CHECK(delegated_compare(5, 5));
+        CHECK_FALSE(delegated_compare(5, 6));
+    }
 }
 
 TEST_CASE("TEST_CASE - function object")
 {
-  std::function<bool(int, int)> compare_func = [&](int a, int b) { return a == b; };
-  auto delegated_compare = delegate(compare_func);
-  CHECK(delegated_compare(5, 5));
-  CHECK_FALSE(delegated_compare(5, 6));
+    const std::function compare_func = [&](const int a, const int b) { return a == b; };
+    auto delegated_compare = delegate(compare_func);
+    CHECK(delegated_compare(5, 5));
+    CHECK_FALSE(delegated_compare(5, 6));
 }
 
 TEST_CASE("TEST_CASE - lambda expression")
 {
-  // lambda needs explicit type
-  auto delegated_compare = delegate<bool(int, int)>(
-      [&](int a, int b) {
-        return a == b;
-      });
+    // lambda needs explicit type
+    auto delegated_compare = delegate<bool(int, int)>([&](const int a, const int b) { return a == b; });
 
-  CHECK(delegated_compare(5, 5));
-  CHECK_FALSE(delegated_compare(5, 6));
+    CHECK(delegated_compare(5, 5));
+    CHECK_FALSE(delegated_compare(5, 6));
 }
 
 TEST_SUITE_END;
--- vtc/delegate.hpp
+++ vtc/delegate.hpp
@@ -11,70 +11,74 @@ template<typename ReturnT, typename... Args>
 class func_wrapper
 {
 public:
-  virtual ~func_wrapper() = default;
-  virtual ReturnT func(Args... args) = 0;
+    virtual ~func_wrapper() = default;
+    virtual ReturnT func(Args... args) = 0;
 };
 
 template<typename T, typename ReturnT, typename... Args>
-class member_func_wrapper : public func_wrapper<ReturnT, Args...>
+class member_func_wrapper final : public func_wrapper<ReturnT, Args...>
 {
 public:
-  member_func_wrapper(T *obj, ReturnT (T::*func)(Args...)) : obj_(obj), func_(func) {}
+    member_func_wrapper(T *obj, ReturnT (T::*func)(Args...)) : obj_(obj), func_(func)
+    {}
 
-  ReturnT func(Args... args) override
-  {
-    return std::invoke(func_, obj_, std::forward<Args>(args)...);
-  }
+    ReturnT func(Args... args) override
+    {
+        return std::invoke(func_, obj_, std::forward<Args>(args)...);
+    }
 
 private:
-  T *obj_;
-  ReturnT (T::*func_)(Args...);
+    T *obj_;
+    ReturnT (T::*func_)(Args...);
 };
 
 template<typename T, typename ReturnT, typename... Args>
-class const_member_func_wrapper : public func_wrapper<ReturnT, Args...>
+class const_member_func_wrapper final : public func_wrapper<ReturnT, Args...>
 {
 public:
-  const_member_func_wrapper(const T *obj, ReturnT (T::*func)(Args...) const) : obj_(obj), func_(func) {}
+    const_member_func_wrapper(const T *obj, ReturnT (T::*func)(Args...) const) : obj_(obj), func_(func)
+    {}
 
-  ReturnT func(Args... args) override
-  {
-    return std::invoke(func_, obj_, std::forward<Args>(args)...);
-  }
+    ReturnT func(Args... args) override
+    {
+        return std::invoke(func_, obj_, std::forward<Args>(args)...);
+    }
 
 private:
-  const T *obj_;
-  ReturnT (T::*func_)(Args...) const;
+    const T *obj_;
+    ReturnT (T::*func_)(Args...) const;
 };
 
 template<typename ReturnT, typename... Args>
-class nonmember_func_wrapper : public func_wrapper<ReturnT, Args...>
+class nonmember_func_wrapper final : public func_wrapper<ReturnT, Args...>
 {
 public:
-  explicit nonmember_func_wrapper(ReturnT (*func)(Args...)) : _func(func) {}
+    explicit nonmember_func_wrapper(ReturnT (*func)(Args...)) : _func(func)
+    {}
 
-  ReturnT func(Args... args) override
-  {
-    return _func(std::forward<Args>(args)...);
-  }
+    ReturnT func(Args... args) override
+    {
+        return _func(std::forward<Args>(args)...);
+    }
 
 private:
-  ReturnT (*_func)(Args...);
+    ReturnT (*_func)(Args...);
 };
 
 template<typename ReturnT, typename... Args>
-class func_obj_wrapper : public func_wrapper<ReturnT, Args...>
+class func_obj_wrapper final : public func_wrapper<ReturnT, Args...>
 {
 public:
-  explicit func_obj_wrapper(std::function<ReturnT(Args...)> func) : func_(func) {}
+    explicit func_obj_wrapper(std::function<ReturnT(Args...)> func) : func_(func)
+    {}
 
-  ReturnT func(Args... args) override
-  {
-    return std::invoke(func_, std::forward<Args>(args)...);
-  }
+    ReturnT func(Args... args) override
+    {
+        return std::invoke(func_, std::forward<Args>(args)...);
+    }
 
 private:
-  std::function<ReturnT(Args...)> func_;
+    std::function<ReturnT(Args...)> func_;
 };
 
 }// namespace details
@@ -86,25 +90,25 @@ template<typename ReturnT, typename... Args>
 class delegate<ReturnT(Args...)>
 {
 public:
-  template<typename T>
-  [[maybe_unused]] delegate(T *obj, ReturnT (T::*func)(Args...)) : wrapper_(
-      std::make_unique<details::member_func_wrapper<T, ReturnT, Args...>>(obj, func))
-  {}
+    template<typename T>
+    [[maybe_unused]] delegate(T *obj, ReturnT (T::*func)(Args...))
+        : wrapper_(std::make_unique<details::member_func_wrapper<T, ReturnT, Args...>>(obj, func))
+    {}
 
-  template<typename T>
-  [[maybe_unused]] delegate(const T *obj, ReturnT (T::*func)(Args...) const) : wrapper_(
-      std::make_unique<details::const_member_func_wrapper<T, ReturnT, Args...>>(obj, func))
-  {}
+    template<typename T>
+    [[maybe_unused]] delegate(const T *obj, ReturnT (T::*func)(Args...) const)
+        : wrapper_(std::make_unique<details::const_member_func_wrapper<T, ReturnT, Args...>>(obj, func))
+    {}
 
-  [[maybe_unused]] explicit delegate(ReturnT(func)(Args...)) : wrapper_(
-      std::make_unique<details::nonmember_func_wrapper<ReturnT, Args...>>(func))
-  {}
+    [[maybe_unused]] explicit delegate(ReturnT(func)(Args...))
+        : wrapper_(std::make_unique<details::nonmember_func_wrapper<ReturnT, Args...>>(func))
+    {}
 
-  explicit delegate(std::function<ReturnT(Args...)> func) : wrapper_(
-      std::make_unique<details::func_obj_wrapper<ReturnT, Args...>>(func))
-  {}
+    explicit delegate(std::function<ReturnT(Args...)> func)
+        : wrapper_(std::make_unique<details::func_obj_wrapper<ReturnT, Args...>>(func))
+    {}
 
-  /*
+    /*
     Does not support automatic type deduction for lambda delegates. Must specify
     the template parameters for the delegate object if using a lambda argument:
 
@@ -116,16 +120,16 @@ public:
     The type of the lambda-expression (which is also the type of the closure object)
     is a unique, unnamed nonunion class type [...]
   */
-  explicit delegate(auto func) : delegate(static_cast<std::function<ReturnT(Args...)>>(func))
-  {}
+    explicit delegate(auto func) : delegate(static_cast<std::function<ReturnT(Args...)>>(func))
+    {}
 
-  ReturnT operator()(Args... args)
-  {
-    return wrapper_->func(std::forward<Args>(args)...);
-  }
+    ReturnT operator()(Args... args)
+    {
+        return wrapper_->func(std::forward<Args>(args)...);
+    }
 
 private:
-  std::unique_ptr<details::func_wrapper<ReturnT, Args...>> wrapper_;
+    std::unique_ptr<details::func_wrapper<ReturnT, Args...>> wrapper_;
 };
 
 template<typename T, typename ReturnT, typename... Args>
