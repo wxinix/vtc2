6dd2369de82d1536283d304a218497133455478d
wxinix <xinwuping007@gmail.com>
2025-03-10 13:10:12 -0400

    Add more test placeholder files.

---
+++ tests/mmu_test.cpp
@@ -0,0 +1,15 @@
+// clang-format off
+#include <doctest/doctest.h>
+// clang-format on
+
+TEST_SUITE_BEGIN("TEST_SUITE vtc::mmu");
+
+TEST_CASE("TEST_CASE - 1")
+{
+    SUBCASE("SUBCASE 1")
+    {
+
+    }
+}
+
+TEST_SUITE_END;
